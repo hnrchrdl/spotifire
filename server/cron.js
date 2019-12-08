137d7a70b96ff6d53f4d616fa46a872f33cfddd2
hnrchrdl <hinrich@posteo.de>
2019-12-08 02:58:55 +0100

    refactor cron jobs

--- server/cron.js
+++ server/cron.js
@@ -4,19 +4,22 @@ const subscriptions = require("./subscriptions");
 const spotify = require("./spotify");
 const { setUser, getUsersBySubscription } = require("./firebase");
 
-const cronJob = async (req, res) => {
-  const { subscriptionId } = req.params;
-  const subscription = subscriptions[subscriptionId];
-  const usersSubscribed = await getUsersBySubscription(subscriptionId);
-  usersSubscribed.forEach(async function(doc) {
+function updatePlaylist(doc, subscription, subscriptionId) {
+  return new Promise(async (resolve, reject) => {
+    console.log(doc, subscription, subscriptionId);
     const user = doc.data();
     const userId = doc.id;
-    const tracks = await subscription.spotifire({
-      userId,
-      accessToken: user.accessToken,
-      refreshToken: user.refreshToken,
-      expiresOn: user.expiresOn
-    });
+    const tracks = await subscription
+      .spotifire({
+        userId,
+        accessToken: user.accessToken,
+        refreshToken: user.refreshToken,
+        expiresOn: user.expiresOn
+      })
+      .catch(e => {
+        console.log(e);
+        reject(e);
+      });
 
     const playlistId = user.subscriptions[subscriptionId].playlistDetails.id;
 
@@ -27,18 +30,25 @@ const cronJob = async (req, res) => {
         refreshToken: user.refreshToken,
         expiresOn: user.expiresOn
       })
-      .catch(e => console.log(e));
+      .catch(e => {
+        console.log(e);
+        reject(e);
+      });
 
     await connection
       .replaceTracksInPlaylist(
         playlistId,
         tracks.map(track => track.uri)
       )
-      .catch(e => console.log(e));
+      .catch(e => {
+        console.log(e);
+        reject(e);
+      });
 
-    const playlist = await connection
-      .getPlaylist(playlistId)
-      .catch(e => console.log(e));
+    const playlist = await connection.getPlaylist(playlistId).catch(e => {
+      console.log(e);
+      reject(e);
+    });
 
     await setUser({
       id: user.id,
@@ -57,14 +67,31 @@ const cronJob = async (req, res) => {
           updatedOn: new Date()
         }
       }
-    }).catch(e => console.log(e));
+    }).catch(e => {
+      console.log(e);
+      reject(e);
+    });
+    resolve();
+  });
+}
+
+const cronJob = async (req, res) => {
+  const { subscriptionId } = req.params;
+  const subscription = subscriptions[subscriptionId];
+  const usersSubscribed = await getUsersBySubscription(subscriptionId);
+  const all = [];
+  usersSubscribed.forEach(doc => {
+    all.push(updatePlaylist(doc, subscription, subscriptionId));
+  });
+  await Promise.all(all).catch(e => {
+    res.status(500).json(e);
   });
   res.status(201).end();
 };
 
 module.exports = server => {
   server.get(
-    "/cron/:playlistId",
+    "/cron/:subscriptionId",
     basicAuth({
       users: {
         [process.env.CRON_USER]: process.env.CRON_PW
